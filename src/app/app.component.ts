56f5a7345ad4ef336d51a26f88a44adfeee5d55f
Jose Asilis <jja3324@g.rit.edu>
2018-06-27 13:52:52 -0400

    Angular Docker Sass, Pug Configuration - Working

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
